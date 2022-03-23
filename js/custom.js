24ff1644d0f8650eb063de0e76d8531d50803438
Santiago Iamarino <santiagoiamarino@gmail.com>
2022-03-23 19:41:16 -0300

    Banner 3

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,16 @@
-// Custom product slider 
 var $ = jQuery
+
+// Header
+
+$(window).scroll(function(){
+    var sticky = $('.sticky-header'),
+        scroll = $(window).scrollTop();
+  
+    if (scroll >= 80) sticky.addClass('fixed-header');
+    else sticky.removeClass('fixed-header');
+});
+
+// Custom product slider 
 
 $('.slider-product.product-2').on('mouseenter', () => {
     $('.slider-product .texture-bg').css({
@@ -31,18 +42,10 @@ $('.menu-toggler').on('click', () => {
     $('header .main-menu').toggleClass('mobile-active');
 })
 
-$(window).scroll(function(){
-    var sticky = $('.sticky-header'),
-        scroll = $(window).scrollTop();
-  
-    if (scroll >= 80) sticky.addClass('fixed-header');
-    else sticky.removeClass('fixed-header');
-  });
-
-  function toggleAboutUsImg(event) {
+function toggleAboutUsImg(event) {
     const targetIndex = $(event.target).data('target');
     const target = $(`#about_us_img_${targetIndex}`);
-    
+
     $(target).toggleClass('active');
 
     if(!$(target).hasClass('no-index')){
@@ -52,7 +55,39 @@ $(window).scroll(function(){
     } else {
         $(target).toggleClass('no-index');
     }
-  }
+}
 
-  $('.about-us .background .background-container .trigger').on('mouseenter', toggleAboutUsImg);
-  $('.about-us .background .background-container .trigger').on('mouseleave', toggleAboutUsImg);
+// About us
+
+$('.about-us .background .background-container .trigger').on('mouseenter', toggleAboutUsImg);
+$('.about-us .background .background-container .trigger').on('mouseleave', toggleAboutUsImg);
+
+// Flotant imgs
+
+const flotantOffsetTop = $('.nuestros-pilares').offset().top - 200;
+
+$(window).scroll(function(){
+    const scroll = $(window).scrollTop();
+  
+    if (scroll >= flotantOffsetTop) {
+        $('.flotant-img').removeClass('initial');
+    }
+});
+
+$('.flotant-img .flotant').on('mouseenter', toggleFlotant);
+$('.flotant-img .flotant').on('mouseleave', toggleFlotant);
+
+function toggleFlotant(event) {
+
+    $('#flotants_section_title').toggleClass('no-show');
+
+    const parent = $(event.target).parents('.flotant-img')[0];
+    const target = $(parent).data('target');
+
+    const title = $(parent).data('title');
+    $('#flotant_section_name').html(title);
+
+    $(`.nuestros-pilares #content_${target}`).toggleClass('active');
+    $(parent).toggleClass('active');
+    
+}
---
+++ sections/banner4.php
@@ -0,0 +1,94 @@
+<section class='nuestros-pilares'>
+    <div class="section-container">
+        <div class="background">
+            <div class="background-container">
+                <img src="./images/08.png" alt="">
+
+                <div class="flotant-imgs">
+                    <div class="flotant-img initial img-1" data-target="1" data-title="Autoerotismo">
+                        <div class="flotant">
+                            <img src="./images/bola.png" alt="">
+                        </div>
+                        <div class="shadow">
+                            <img src="./images/bola-sombra.png" alt="">
+                        </div>
+                        <div class="text">
+                            <p>Autoerotismo</p>
+                        </div>
+                    </div>
+
+                    <div class="flotant-img initial img-2" data-target="2" data-title="Skinfood">
+                        <div class="flotant">
+                            <img src="./images/triangulo-invertido.png" alt="">
+                        </div>
+                        <div class="shadow">
+                            <img src="./images/triangulo-invertido-sombra.png" alt="">
+                        </div>
+                        <div class="text">
+                            <p>Skinfood</p>
+                        </div>
+                    </div>
+
+                    <div class="flotant-img initial img-3" data-target="3" data-title="Sustentabilidad">
+                        <div class="flotant">
+                            <img src="./images/Grupo-114.png" alt="">
+                        </div>
+                        <div class="shadow">
+                            <img src="./images/bola-sombra-1.png" alt="">
+                        </div>
+                        <div class="text">
+                            <p>Sustentabilidad</p>
+                        </div>
+                    </div>
+                </div>
+               
+
+            </div>
+        </div>
+
+        <div class="main-content">
+            <p class="section-name" id="flotant_section_name">
+                NUESTROS PILARES
+            </p>
+
+            <div class="section-title" id='flotants_section_title'>
+                <h2>La piel se alimenta</h2>
+            </div>
+
+            <div class="contents">
+                <div class="content" id="content_1">
+                    <div class="d-flex">
+                        <p>El ritual con unx.</p>
+                        <p>La unión de lo profano con lo sagrado</p>
+                    </div>
+                    
+                    <p class='third'>El contacto abre las puertas al placer de vivir,</p>
+                    <p>de sentir, de ser con unx y lxs otrxs.</p>
+                </div>
+
+                <div class="content" id="content_2">
+                    <p>La piel está viva, se nutre y se alimenta</p>
+                    <div class="d-flex">
+                        <p>Alimento y placer</p>
+                        <p>noble, natural y nutritivo</p>
+                    </div>
+                   
+                    <p>Escuchar la piel</p>
+                </div>
+
+                <div class="content" id="content_3">
+                    <div class="d-flex">
+                        <p>Estamos hechos de naturaleza</p>
+                        <p>Union entre el afuera y el adentro</p>
+                    </div>
+                    <p class='third'>Consciencia de nuestra huella</p>
+                    <div class="d-flex">
+                        <p>Amor universal</p>
+                        <p>El equilibrio para bien de todos</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
